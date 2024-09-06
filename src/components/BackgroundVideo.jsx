299e1527a3cb514cc33c69c3393807e562bd3807
gatsuweb <ivan.duran@outlook.fr>
2024-09-06 14:45:44 +0200

    fix vid

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -1,10 +1,11 @@
+import Vidback from "../assets/galerie/vid4.mov"
 import "../styles/BackgroundVideo.css";
 
 const BackgroundVideo = () => {
   return (
     <div className="video-background">
       <video autoPlay loop muted>
-        <source src="/src/assets/galerie/vid4.mov" type="video/mp4" />
+        <source src={Vidback} type="video/mp4" />
       </video>
     </div>
   );
