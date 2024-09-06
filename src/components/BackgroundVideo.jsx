0e808b204b7c5c6643a01055c876578edd8e35d9
gatsuweb <ivan.duran@outlook.fr>
2024-09-06 15:25:21 +0200

    correction

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -1,11 +1,10 @@
-import Vidback from "../assets/galerie/vid4.mov"
 import "../styles/BackgroundVideo.css";
 
 const BackgroundVideo = () => {
   return (
     <div className="video-background">
       <video autoPlay loop muted>
-        <source src={Vidback} type="video/mp4" />
+        <source src="/src/assets/galerie/vid4.mp4" type="video/mp4" />
       </video>
     </div>
   );
