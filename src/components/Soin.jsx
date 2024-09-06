3976c9dbc857deadccba62897452d9d6209ce653
gatsuweb <ivan.duran@outlook.fr>
2024-09-06 19:05:08 +0200

    correction orthographe

--- src/components/Soin.jsx
+++ src/components/Soin.jsx
@@ -1,30 +1,38 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
-import "../styles/Soin.css"
-import FicheSoin from "../assets/contact/fiche_de_soin.jpg"
+import "../styles/Soin.css";
+import FicheSoin from "../assets/contact/fiche_de_soin.jpg";
 
 function Soin(props) {
-    // const handleDownload = () => {
-    //     const imagePath = '../assets/contact/fiche_de_soin.jpg';
-    //     const link = document.createElement('a');
-    //     link.href = imagePath;
-    //     link.download = 'fiche_de_soin.jpg';
-    //     link.click();
-    //     document.body.removeChild(link);
-    //   };
+    const { soinRef } = props;
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const {soinRef} = props
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+
     return (
         <section className="soinContainer" ref={soinRef}>
             <div className="soinContent">
                 <h2>SOIN</h2>
-                {/* <button onClick={handleDownload} style={{ marginLeft: '10px' }}>
-          Télécharger l'image
-        </button> */}
-                   <img src={FicheSoin} alt="" className="ficheSoin"/>
+                <img 
+                    src={FicheSoin} 
+                    alt="Fiche de soin" 
+                    className="ficheSoin"
+                    onClick={openModal}
+                />
+                {isModalOpen && (
+                    <div className="modalOverlay" onClick={closeModal}>
+                        <div className="modalContent" onClick={(e) => e.stopPropagation()}>
+                            <img src={FicheSoin} alt="Fiche de soin" />
+                            <button className="closeModal" onClick={closeModal}>×</button>
+                        </div>
+                    </div>
+                )}
             </div>
         </section>
-    )
+    );
 }
+
 Soin.propTypes = {
     soinRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object])
 };
