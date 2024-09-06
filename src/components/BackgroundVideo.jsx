0ff697eab3798970ee52fd721cb6be5fc372b729
gatsuweb <ivan.duran@outlook.fr>
2024-09-06 15:41:58 +0200

    video bug

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -1,10 +1,11 @@
+import VidBack from "../assets/vid4.mp4";
 import "../styles/BackgroundVideo.css";
 
 const BackgroundVideo = () => {
   return (
     <div className="video-background">
       <video autoPlay loop muted>
-        <source src="/src/assets/galerie/vid4.mp4" type="video/mp4" />
+        <source src={VidBack} type="video/mp4" />
       </video>
     </div>
   );
