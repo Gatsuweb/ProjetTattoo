37a762f2c1c760839c6cbecbc1fd6f3e5b91a989
gatsuweb <ivan.duran@outlook.fr>
2024-09-07 12:48:28 +0200

    video débug

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -1,15 +1,18 @@
+import VideoMp4 from "../assets/vid4.mp4";
+import VideoWebm from "../assets/vid4.webm";
+import FallbackImage from "../assets/fallback.png";
 import "../styles/BackgroundVideo.css";
 
 const BackgroundVideo = () => {
   return (
     <div className="video-background">
     <video autoPlay muted loop className="background-video">
-      <source src="/src/assets/vid4.mp4" type="video/mp4" />
-      <source src="/src/assets/vid4.webm" type="video/webm" />
+      <source src={VideoMp4} type="video/mp4" />
+      <source src={VideoWebm} type="video/webm" />
       Votre navigateur ne supporte pas la lecture des vidéos.
     </video>
     <img
-        src="/src/assets/fallback.png"
+        src={FallbackImage}
         alt="Image de fallback"
         className="fallback-image"
       />
