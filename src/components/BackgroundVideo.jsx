3e15d0ab8a657903c933fd17abb677131de5a55b
gatsuweb <ivan.duran@outlook.fr>
2024-09-07 11:39:30 +0200

    video bug

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -4,7 +4,7 @@ const BackgroundVideo = () => {
   return (
     <div className="video-background">
       <video autoPlay loop muted>
-        <source src="/src/assets/galerie/vid4.mov" type="video/mp4" />
+        <source src="/src/assets/galerie/vid4.mp4" type="video/mp4" />
         <source src="/src/assets/galerie/vid4.webm" type="video/webm" />
       </video>
     </div>
