62ceff4aec1c9f93b4b077708646e6f56932876c
gatsuweb <ivan.duran@outlook.fr>
2024-09-07 12:37:08 +0200

    video bug

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -4,12 +4,12 @@ const BackgroundVideo = () => {
   return (
     <div className="video-background">
     <video autoPlay muted loop className="background-video">
-      <source src="/src/assets/galerie/vid4.mp4" type="video/mp4" />
-      <source src="/src/assets/galerie/vid4.webm" type="video/webm" />
+      <source src="/src/assets/vid4.mp4" type="video/mp4" />
+      <source src="/src/assets/vid4.webm" type="video/webm" />
       Votre navigateur ne supporte pas la lecture des vidéos.
     </video>
     <img
-        src="/src/assets/galerie/fallback.png"
+        src="/src/assets/fallback.png"
         alt="Image de fallback"
         className="fallback-image"
       />
