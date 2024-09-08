954af2e3d79c21b4c4ee8fe2955d67b83204bde8
gatsuweb <ivan.duran@outlook.fr>
2024-09-08 10:44:39 +0200

    background image

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 
-import VideoMp4 from "../assets/vid4.mp4";
+import VideoMp4 from "../assets/vid4.mov";
 import VideoWebm from "../assets/vid4.webm";
 // import FallbackImage from "../assets/fallback.png";
 import "../styles/BackgroundVideo.css";
@@ -17,11 +17,11 @@ const BackgroundVideo = () => {
       });
     }
   }, []);
-
+  
   return (
     <div className="video-background">
-    <video ref={videoRef} autoPlay muted loop playsInline controls={false}  className="background-video">
-      <source src={VideoMp4} type="video/mp4" />
+    <video autoPlay muted loop playsInline controls={false}  className="background-video">
+      <source src={VideoMp4} type="video/mov" />
       <source src={VideoWebm} type="video/webm" />
       Votre navigateur ne supporte pas la lecture des vidéos.
     </video>
