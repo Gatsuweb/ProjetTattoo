d1807576cda6192e0fb95dd12745d211017cfd6b
DURAN Ivan <ivan.duran@outlook.fr>
2025-04-22 21:35:10 +0200

    seo pages

--- src/pages/Flash.jsx
+++ src/pages/Flash.jsx
@@ -1,8 +1,9 @@
 import NavBar from "../components/NavBar";
-import "../styles/Flash.css";
+import { Helmet } from "react-helmet";
 import Modal from 'react-modal';
 import GalerieFlash from "../components/GalerieFlash";
 import { CarousselFlash } from "../components/CarousselFlash";
+import "../styles/Flash.css";
 
 
 Modal.setAppElement('#root');
@@ -12,10 +13,17 @@ function Flash() {
 
     return (
         <>
+        <Helmet>
+        <title>Flashs disponibles - Valkyr Ink Tattoo</title>
+        <meta 
+          name="description" 
+          content="Flashs de tatouages uniques et originaux créés par Valkyr Ink à Plévin, à proximité de Carhaix. Réservez dès maintenant votre coup de cœur !"
+        />
+      </Helmet>
             <section className="container-flash">
                 <NavBar />
                 <h1>Galerie des Flashs</h1>
-            <CarousselFlash />
+                <CarousselFlash />
             <div >
                 <GalerieFlash />
            
--- src/pages/Galerie.jsx
+++ src/pages/Galerie.jsx
@@ -1,14 +1,24 @@
+import { Helmet } from "react-helmet";
 import NavBar from "../components/NavBar";
 import Test from "../components/Test";
 
 function Galerie() {
 
   return (
+    <>
+         <Helmet>
+        <title>Galerie de tatouages - Valkyr Ink Tattoo</title>
+        <meta 
+          name="description" 
+          content="Découvrez la galerie des œuvres de Valkyr Ink Tattoo à Plévin. Tatouages et créations uniques en noirs et en couleurs."
+        />
+      </Helmet>
+
     <section className="galerieContainer">
         <NavBar />
         <Test />
     </section>
-
+    </>
   );
 }
 
