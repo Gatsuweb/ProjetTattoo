cbba8ead52f0f6a5bab9bcd9a08fc3665b398665
DURAN Ivan <ivan.duran@outlook.fr>
2025-04-22 17:02:50 +0200

    lien interne

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,7 @@ import Mel2 from "../assets/mel2.jpg"
 import ImgService from "../assets/cellela.jpg"
 import ImgService2 from "../assets/imgmoiservice.jpg"
 import Gris5 from "../assets/galerie/cheville.jpg"
+import { Link } from "react-router-dom";
 import ImgService3 from '../assets/imgServices2.jpg'
 
 
@@ -20,20 +21,20 @@ const About = React.forwardRef((props, ref) => {
                     <img src={Mel2} alt="" />
                     <p>« Hey Mel, tu sais dessiner. Tu pourrais me tatouer ? » voilà comment cette folle aventure a commencé. <br/><br/>
 
-                        Je m’appelle Mélanie, j’ai 26 ans et d’aussi loin que je me souvienne j’ai toujours eu un pinceau ou un crayon entre les mains. <br/>
-                        J’ai énormément d’inspiration artistique et une grande soif de création. En art plastique j’avais tellement d’idées que la prof m’envoyait les élèves qui étaient en panne d’inspiration.<br/><br/> 
-                        Je n’ai pas pu faire mes études dans l’art et me suis donc orientée vers des études littéraires, puis une fac d’anglais. J’ai rapidement arrêté parce que je m’ennuyais. J’ai travaillé dans pleins de domaines différents jusqu’à ce qu’un de mes amis vienne en vacances chez moi avec une machine à tatouer qu’il avait achetée pour le délire et dont il ne savait pas se servir.<br/>
-                        Et là, grosse révélation ! J’avais enfin trouvé ma voie. Je me suis donc beaucoup entraînée (merci à toutes ces oranges, à toutes ces fausses peaux mais aussi à mes nombreux cobayes, je ne vous oublie pas) et j’ai passé ma formation hygiène et salubrité en décembre 2023. 
-                        J’ai pu bénéficier des précieux conseils et de la connaissance d’excellents tatoueurs avant d’enfin me décider et de me lancer à mon compte.<br/><br/>
-                        <span>Pourquoi Valkyr’Ink ? </span><br/><br/>
+                        Je m&apos;appelle Mélanie, j&apos;ai 26 ans et d&apos;aussi loin que je me souvienne j&apos;ai toujours eu un pinceau ou un crayon entre les mains. <br/>
+                        J&apos;ai énormément d&apos;inspiration artistique et une grande soif de création. En art plastique j&apos;avais tellement d&apos;idées que la prof m&apos;envoyait les élèves qui étaient en panne d&apos;inspiration.<br/><br/> 
+                        Je n&apos;ai pas pu faire mes études dans l&apos;art et me suis donc orientée vers des études littéraires, puis une fac d&apos;anglais. J&apos;ai rapidement arrêté parce que je m&apos;ennuyais. J&apos;ai travaillé dans pleins de domaines différents jusqu&apos;à ce qu&apos;un de mes amis vienne en vacances chez moi avec une machine à tatouer qu&apos;il avait achetée pour le délire et dont il ne savait pas se servir.<br/>
+                        Et là, grosse révélation ! J&apos;avais enfin trouvé ma voie. Je me suis donc beaucoup entraînée (merci à toutes ces oranges, à toutes ces fausses peaux mais aussi à mes nombreux cobayes, je ne vous oublie pas) et j&apos;ai passé ma formation hygiène et salubrité en décembre 2023. 
+                        J&apos;ai pu bénéficier des précieux conseils et de la connaissance d&apos;excellents tatoueurs avant d&apos;enfin me décider et de me lancer à mon compte.<br/><br/>
+                        <span>Pourquoi Valkyr&apos;Ink ? </span><br/><br/>
 
-                        J’ai longuement réfléchi à mon nom d’artiste et le nom m’est venu en tête d’un coup;
-                        « Valkyr’Ink ».
-                        La Valkyrie, la femme guerrière, pour ne jamais oublié tout ce que j’ai traversé dans ma vie, pour m’inspirer à toujours me battre pour atteindre mes buts et me servir de toutes mes batailles comme d’une force et d’un moteur pour atteindre mes rêves et devenir la meilleure version possible de moi-même. Mais aussi en hommage à chaque guerrier qui sommeil en chacun de nous, en chaque homme, en chaque femme. Pour nous rappeler que peu importe ce qu’on traverse il faut se battre pour avoir la vie qui nous inspire. 
+                        J&apos;ai longuement réfléchi à mon nom d&apos;artiste et le nom m&apos;est venu en tête d&apos;un coup;
+                        « Valkyr&apos;Ink ».
+                        La Valkyrie, la femme guerrière, pour ne jamais oublié tout ce que j&apos;ai traversé dans ma vie, pour m&apos;inspirer à toujours me battre pour atteindre mes buts et me servir de toutes mes batailles comme d&apos;une force et d&apos;un moteur pour atteindre mes rêves et devenir la meilleure version possible de moi-même. Mais aussi en hommage à chaque guerrier qui sommeil en chacun de nous, en chaque homme, en chaque femme. Pour nous rappeler que peu importe ce qu&apos;on traverse il faut se battre pour avoir la vie qui nous inspire. 
                         <br/><br/>
-                        Et quoi de mieux qu’un tatouage pour marquer toutes ces épreuves, toutes ces expériences, tous ces moments de vie afin de ne jamais oublier d’où nous venons et où nous avons envie d’aller. Encrer en vous toutes ces batailles est vraiment un honneur et le plus beau métier dont je pouvais rêver. 
+                        Et quoi de mieux qu&apos;un tatouage pour marquer toutes ces épreuves, toutes ces expériences, tous ces moments de vie afin de ne jamais oublier d&apos;où nous venons et où nous avons envie d&apos;aller. Encrer en vous toutes ces batailles est vraiment un honneur et le plus beau métier dont je pouvais rêver. 
                         <br/><br/>
-                        Merci de m’inspirer chaque jour avec vos projets plus beaux les uns que les autres.
+                        Merci de m&apos;inspirer chaque jour avec vos projets plus beaux les uns que les autres.
                     </p>
                 </div>
             </div>
@@ -55,8 +56,8 @@ const About = React.forwardRef((props, ref) => {
             <div className="aboutContent service" ref={anotherRef}>
                 <div id="service-txt">
                     <h2>Services</h2>
-                    <p>Artiste tatoueuse réalisant des œuvres aux formes et styles variés sur la peau, en déposant de l’encre pigmentée à l’aide d’un dermographe.
-                    Cet appareil électrique est composé d’une aiguille qui dépose l’encre entre le derme et l’épiderme, entre 2 et 4 millimètres de profondeur.
+                    <p>Artiste tatoueuse à <Link to="/tatoueuse-carhaix" style={{color: "white", textDecoration: "none"}}>Plévin à côté de Carhaix</Link> réalisant des œuvres aux formes et styles variés sur la peau, en déposant de l&apos;encre pigmentée à l&apos;aide d&apos;un dermographe.
+                    Cet appareil électrique est composé d&apos;une aiguille qui dépose l&apos;encre entre le derme et l&apos;épiderme, entre 2 et 4 millimètres de profondeur.
                     <br />
                     Je tatoue principalement en noir et gris. Les motifs colorés sont au cas par cas.<br />
                     Je ne pratique pas la pose de piercings.
