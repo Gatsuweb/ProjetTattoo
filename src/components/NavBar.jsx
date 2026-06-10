0c299effb1ad643f30a614dd642dfb64e39ad119
gatsuweb <ivan.duran@outlook.fr>
2026-06-10 16:07:14 +0200

    correction nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,29 +9,35 @@ function NavBar() {
     const toggleMenu = () => {
       setIsOpen(!isOpen);
     };
+
+    const closeMenu = () => {
+      setIsOpen(false);
+    };
   
     return (
       <section className="navbar">
-        <Link to="/home" className="navLink">
+        <Link to="/home" className="navLink" onClick={closeMenu}>
         <div className="Logo">Valkyr Ink</div>
         </Link>
         <div className={`pages ${isOpen ? "open" : ""}`}>
           <ul>
-            <Link to="/home" className="navLink">
+            <Link to="/home" className="navLink" onClick={closeMenu}>
             <li>Accueil</li>
             </Link>
-            <Link to="/galerie" className="navLink">
+            <Link to="/galerie" className="navLink" onClick={closeMenu}>
             <li>Galerie</li>
             </Link>
-            <Link to="/flash" className="navLink">
+            <Link to="/flash" className="navLink" onClick={closeMenu}>
             <li>Flash</li>
             </Link>
-            <Link to="/event" className="navLink" >
+            <Link to="/event" className="navLink" onClick={closeMenu}>
             <li>Evenements</li>
             </Link>
           </ul>
         </div>
-            <div className="burger" onClick={toggleMenu}><img src= {IconeBurger} alt="" className="iconeburger"/></div>
+            <button type="button" className="burger" onClick={toggleMenu} aria-label={isOpen ? "Fermer le menu" : "Ouvrir le menu"} aria-expanded={isOpen}>
+              <img src={IconeBurger} alt="" className="iconeburger" />
+            </button>
       </section>
     )
 }
